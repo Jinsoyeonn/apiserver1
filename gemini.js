4bdeeaffaf650c979940c90bf20b7eedaea5ccb6
Jinsoyeonn <wlsdmsquf@nate.com>
2024-05-21 16:13:33 +0900

    Update gemini.js

--- gemini.js
+++ gemini.js
@@ -44,3 +44,15 @@ class ApiProvider {
 }
 
 module.exports = ApiProvider;
+
+var express = require("express");
+var app = express();
+
+app.get("/", (req, res) => {
+    res.send(
+        "<p>안녕하세요</p>"
+    )
+})
+app.listen(3000, (err)=> {
+    console.log(3000);
+})
