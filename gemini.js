0435195fbf529e91815bb94148b95d0c4f5ef76b
Jinsoyeonn <wlsdmsquf@nate.com>
2024-05-21 16:01:18 +0900

    Add files via upload

---
+++ gemini.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+
+class ApiProvider {
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+
+  async getRestToken(applicationId, privateKey) {
+    const payload = {
+      application_id: applicationId,
+      private_key: privateKey
+    };
+
+    const url = `${this.baseUrl}/v2/request/token`;
+    const response = await axios.post(url, payload, {
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
+
+    return response.data;
+  }
+
+  async getEasyPayUserToken(token, user) {
+    const payload = {
+      user_id: user.id,
+      email: user.email,
+      name: user.username,
+      gender: user.gender,
+      birth: user.birth,
+      phone: user.phone,
+    };
+
+    const url = `${this.baseUrl}/v2/request/user/token`;
+    const response = await axios.post(url, payload, {
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    });
+
+    return response.data;
+  }
+}
+
+module.exports = ApiProvider;
